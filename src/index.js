126e2e49595e19719e0c18816bf73987dd60394d
Michail Bereza <berezama@gmail.com>
2017-09-19 17:38:30 +0800

    fix eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 import readLineSync from 'readline-sync';
 
-export const welcome = () => {
+const welcome = () => {
 	console.log('\nWelcome to the Brain Games! \n');
 	const username = readLineSync.question('May I have your name? ');
 	console.log(`Hello, ${username}!`);
 };
+
+export default welcome;
