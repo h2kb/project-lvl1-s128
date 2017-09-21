24830be6bdada9bb420883bed6be2651f98781da
Michail Bereza <berezama@gmail.com>
2017-09-22 00:51:41 +0800

    fix brain-even

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { gameEven } from '..';
+import gameEven from '..';
 
 gameEven();
--- src/index.js
+++ src/index.js
@@ -1,48 +1,36 @@
 #!/usr/bin/env node
 import readLineSync from 'readline-sync';
 
-export const greeting = () => '\nWelcome to the Brain Games!';
-
 const randNum = (min, max) => {
   const num = (Math.random() * (max - min)) + min;
 
   return Math.floor(num);
 };
 
-const getUserName = () => {
-  const userName = readLineSync.question('May I have your name? ');
-  return userName;
-};
-
-const getUserAnswer = () => {
-  const userAnswer = readLineSync.question('Your answer: ');
-  return userAnswer;
-};
-
-export const gameEven = () => {
-  console.log(greeting());
+const gameEven = () => {
+  console.log('\nWelcome to the Brain Games!');
   console.log('Answer "yes" if number even otherwise answer "no".\n');
-  const name = getUserName();
+  const name = readLineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!\n`);
-  let acc = 0;
 
-  for (let i = 1; i <= 3; i += 1) {
-    const number = randNum(0, 100);
-    console.log(`Question: ${number}`);
-    const answer = getUserAnswer();
-    if (number % 2 === 0 && answer === 'yes') {
-      console.log('Correct!');
-      acc += 1;
-    } else if (number % 2 !== 0 && answer === 'no') {
-      console.log('Correct!');
-      acc += 1;
+  const flow = (n) => {
+    if (n === 0) {
+      console.log(`\nCongratulations, ${name}!`);
     } else {
-      console.log(`'${answer}' is wrong answer ;(. 
-        Correct answer was 'no'.\nLet's try again, ${name}!\n`);
-      break;
+      const number = randNum(0, 100);
+      console.log(`Question: ${number}`);
+      const correctAnswer = (number % 2 === 0) ? 'yes' : 'no';
+      const userAnswer = readLineSync.question('Your answer: ');
+      if (correctAnswer !== userAnswer) {
+        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!\n`);
+      } else {
+        console.log('Correct!');
+        return flow(n - 1);
+      }
     }
-  }
-  if (acc === 3) {
-    console.log(`\nCongratulations, ${name}!`);
-  }
+  };
+
+  return flow(3);
 };
+
+export default gameEven;
