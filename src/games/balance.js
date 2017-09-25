eda4345e55da42caffe68636f08e74af34cb22a0
Michail Bereza <berezama@gmail.com>
2017-09-25 21:34:26 +0800

    add brain-balance

--- src/games/balance.js
+++ src/games/balance.js
@@ -1,5 +1,41 @@
-// import { cons } from 'hexlet-pairs';
-// import startGame from '..';
-// import getRandNum from '../utils';
+import { cons } from 'hexlet-pairs';
+import startGame from '..';
+import getRandNum from '../utils';
 
-// const description = 'Balance the given number.';
+const description = 'Balance the given number.';
+
+const sumOfSequenceOfNum = (numStr) => {
+  let result = 0;
+
+  for (let i = 0; i < numStr.length; i += 1) {
+    result += Number(numStr[i]);
+  }
+
+  return result;
+};
+
+const balanceNum = (num) => {
+  const sumOfNum = sumOfSequenceOfNum(String(num));
+  const lengthOfNum = String(num).length;
+
+  const iter = (sum, count, acc) => {
+    if (count === 0) {
+      return acc;
+    }
+    const nextIterNum = Math.floor(sum / count);
+    const result = acc + String(nextIterNum);
+
+    return iter(sum - nextIterNum, count - 1, result);
+  };
+
+  return iter(sumOfNum, lengthOfNum, '');
+};
+
+const gameData = () => {
+  const question = getRandNum(100, 400);
+  const correctAnswer = balanceNum(question);
+
+  return cons(question, correctAnswer);
+};
+
+export default () => startGame(description, gameData);
